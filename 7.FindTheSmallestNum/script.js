dafd6b4d5d920b732d3c4a9c5e79e073a98958de
ankit9600 <ankitmor1811@gmail.com>
2026-04-27 12:48:40 +0530

    added

--- 7.FindTheSmallestNum/script.js
+++ 7.FindTheSmallestNum/script.js
@@ -1,20 +1,10 @@
-// first method :-
-
-const num = [10,11,12,8,4,3,7,5,14];
-
-const smallNum = Math.min(...num);
-
-console.log(smallNum);
-
-// second method :-
-
 const number = [10,11,12,8,4,3,7,5,14];
 
 let small = number[0];
 
-for(let i = 1; i < number.length; i++){ 
+for(let i = 1; i < number.length; i++){
     if(number[i] < small){
-      small = number[i];
+        small = number[i];
     }
 }
 
